359b4296bed55c083af2acf1a548025231976359
Yusuf Karim <ykarim250@gmail.com>
2018-05-06 13:27:06 -0400

    Working UI

--- src/ui/app/PhotoFind.java
+++ src/ui/app/PhotoFind.java
@@ -1,7 +1,12 @@
 package ui.app;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class PhotoFind extends Application {
 
@@ -10,7 +15,22 @@ public class PhotoFind extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage stage) {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("../../res/fxml/dashboard/Dashboard.fxml"));
+
+        Parent root = null;
+        try {
+            root = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Scene scene = new Scene(root, 600, 400);
+
+        stage.setTitle("PhotoFind");
+        stage.setScene(scene);
+        stage.show();
 
     }
 }
