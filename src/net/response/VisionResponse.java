99057be6f19cb14af420631a071061a11fde71bc
Yusuf Karim <ykarim250@gmail.com>
2018-05-10 15:08:40 -0400

    Parse response of vision request

---
+++ src/net/response/VisionResponse.java
@@ -0,0 +1,9 @@
+package net.response;
+
+import org.apache.http.StatusLine;
+
+public interface VisionResponse {
+
+    StatusLine getStatus();
+//    Object getResponseData();
+}
---
+++ src/net/response/data/PictureDescriptor.java
@@ -0,0 +1,65 @@
+package net.response.data;
+
+import media.descriptors.Description;
+import media.descriptors.Tag;
+
+import java.util.ArrayList;
+
+@Deprecated
+//Unused as it would lead to overabundance of files to represent diff types of responses
+public class PictureDescriptor {
+
+    //List of Tags associated with picture
+    private ArrayList<Tag> tags = new ArrayList<>();
+    //Description of picture
+    private Description description;
+
+    public PictureDescriptor(ArrayList<Tag> tags) {
+        this.tags = tags;
+    }
+
+    public PictureDescriptor(ArrayList<Tag> tags, Description description) {
+        this.tags = tags;
+        this.description = description;
+    }
+
+    public ArrayList<Tag> getTags() {
+        return tags;
+    }
+
+    public Description getDescription() {
+        return description;
+    }
+
+    public void addTag(String tagName, double confidence) {
+        tags.add(new Tag(tagName, confidence));
+    }
+
+    public void addTag(Tag tag) {
+        tags.add(tag);
+    }
+
+    public void removeTag(Tag tag) {
+        tags.remove(tag);
+    }
+
+    public void removeTag(String tagName) {
+        for (Tag tag : tags) {
+            if (tag.getName().equals(tagName)) {
+                tags.remove(tag);
+            }
+        }
+    }
+
+    public void addTags(ArrayList<Tag> tags) {
+        this.tags = tags;
+    }
+
+    public void addDescription(Description description) {
+        this.description = description;
+    }
+
+    public void deleteDescription() {
+        description = new Description(new ArrayList<>(), new ArrayList<>());
+    }
+}
